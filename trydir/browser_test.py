a2e109fea353e5f4911de8433389ce6675517d80
zengln <zengln@zln.com>
2018-02-28 19:50:33 +0800

    更新支持登陆后自动发动态

--- trydir/browser_test.py
+++ trydir/browser_test.py
@@ -5,6 +5,7 @@
 # 一个 selenium + chromeDriver 的尝试
 
 import os
+import time
 from selenium import webdriver
 
 # 引入 chromedriver.exe
@@ -21,7 +22,14 @@ browser.get(url)
 browser.implicitly_wait(10)
 browser.find_element_by_id("loginname").send_keys("username")
 browser.find_element_by_name("password").send_keys("password")
-# browser.find_element_by_id("su").click()
 browser.find_element_by_class_name("btn_32px").click()
+
+time.sleep(5)
+browser.get("https://weibo.com/")
+
+browser.implicitly_wait(10)
+browser.find_elements_by_class_name("W_input")[1].send_keys("Hello Selenium")
+browser.find_element_by_class_name("btn_30px").click()
+
 # 关闭浏览器
 # browser.quit()
