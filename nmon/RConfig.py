e63e570796a92e306abc27cf8ff896ad1936e152
zengln <zengln@zln.com>
2019-08-04 14:28:55 +0800

    记在config配置文件信息到内存

--- nmon/RConfig.py
+++ nmon/RConfig.py
@@ -1,1 +1,18 @@
 # -*- coding:utf-8 -*-
+
+import configparser
+
+
+class Config(object):
+
+    def __init__(self):
+        self.conf = configparser.ConfigParser()
+        self.conf.read(".\\config\\config.ini", encoding="utf-8-sig")
+
+    def reload_all_value(self):
+        sections = self.conf.sections()
+        for section in sections:
+            items = self.conf.items(section)
+            for item in items:
+                self.__setattr__(item[0], item[1])
+
