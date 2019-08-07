685e6e9bd1791865b65f204a14a91bf33d7ee419
zengln <zengln@zln.com>
2019-08-07 22:47:29 +0800

    统一日志输出口

---
+++ nmon/NmonLog.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+# create: 2019-07-29
+# author:zengln
+# desc: 日志输出
+
+import logging
+
+class nmonlog(object):
+
+    @classmethod
+    def init_log(cls):
+        if cls.logger is not None:
+            return cls.logger
+        logger = logging.Logger("nmon_analyse")
+        handler_std = logging.StreamHandler()
+        handler_log = logging.FileHandler(filename="error.log")
+
+        logger.setLevel(logging.DEBUG)
+        handler_std.setLevel(logging.INFO)
+        handler_log.setLevel(logging.ERROR)
+
+        formatter = logging.Formatter("%(asctime)s %(name)s:%(levelname)s:%(message)s",datefmt="%Y-%m-%d %H:%M:%S")
+        handler_std.setFormatter(formatter)
+        handler_log.setFormatter(formatter)
+
+        logger.addHandler(handler_std)
+        logger.addHandler(handler_log)
+        cls.logger = logger
+
+
+
