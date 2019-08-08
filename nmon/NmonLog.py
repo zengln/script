136c4c130c0b8e225ed45e8ac5446fefdf2e1ef9
zengln <zengln@zln.com>
2019-08-08 20:28:53 +0800

    全局的Log类变为全局统一引入log变量,打印日志

--- nmon/NmonLog.py
+++ nmon/NmonLog.py
@@ -7,31 +7,6 @@
 import logging
 
 
-# class nmonlog(object):
-#     log = None
-#
-#     @classmethod
-#     def init_log(cls):
-#         if cls.log is None:
-#             logger = logging.Logger("nmon_analyse")
-#             handler_std = logging.StreamHandler()
-#             handler_log = logging.FileHandler(filename="error.log", encoding="utf-8")
-#
-#             logger.setLevel(logging.DEBUG)
-#             handler_std.setLevel(logging.INFO)
-#             handler_log.setLevel(logging.ERROR)
-#
-#             formatter = logging.Formatter("%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-#             handler_std.setFormatter(formatter)
-#             handler_log.setFormatter(formatter)
-#
-#             logger.addHandler(handler_std)
-#             logger.addHandler(handler_log)
-#             cls.log = logger
-#
-#         return cls.log
-
-
 def get_log():
     logger = logging.Logger("nmon_analyse")
     handler_std = logging.StreamHandler()
