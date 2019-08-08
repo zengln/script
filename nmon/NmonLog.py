a60fc416db90aec071495b5e774e7853fc40ebb9
zengln <zengln@zln.com>
2019-08-08 20:27:50 +0800

    Log 类改为 Log 全局变量

--- nmon/NmonLog.py
+++ nmon/NmonLog.py
@@ -7,28 +7,48 @@
 import logging
 
 
-class nmonlog(object):
-    log = None
+# class nmonlog(object):
+#     log = None
+#
+#     @classmethod
+#     def init_log(cls):
+#         if cls.log is None:
+#             logger = logging.Logger("nmon_analyse")
+#             handler_std = logging.StreamHandler()
+#             handler_log = logging.FileHandler(filename="error.log", encoding="utf-8")
+#
+#             logger.setLevel(logging.DEBUG)
+#             handler_std.setLevel(logging.INFO)
+#             handler_log.setLevel(logging.ERROR)
+#
+#             formatter = logging.Formatter("%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+#             handler_std.setFormatter(formatter)
+#             handler_log.setFormatter(formatter)
+#
+#             logger.addHandler(handler_std)
+#             logger.addHandler(handler_log)
+#             cls.log = logger
+#
+#         return cls.log
 
-    @classmethod
-    def init_log(cls):
-        if cls.log is None:
-            logger = logging.Logger("nmon_analyse")
-            handler_std = logging.StreamHandler()
-            handler_log = logging.FileHandler(filename="error.log", encoding="utf-8")
 
-            logger.setLevel(logging.DEBUG)
-            handler_std.setLevel(logging.INFO)
-            handler_log.setLevel(logging.ERROR)
+def get_log():
+    logger = logging.Logger("nmon_analyse")
+    handler_std = logging.StreamHandler()
+    handler_log = logging.FileHandler(filename="error.log", encoding="utf-8")
 
-            formatter = logging.Formatter("%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-            handler_std.setFormatter(formatter)
-            handler_log.setFormatter(formatter)
+    logger.setLevel(logging.DEBUG)
+    handler_std.setLevel(logging.INFO)
+    handler_log.setLevel(logging.ERROR)
 
-            logger.addHandler(handler_std)
-            logger.addHandler(handler_log)
-            cls.log = logger
+    formatter = logging.Formatter("%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+    handler_std.setFormatter(formatter)
+    handler_log.setFormatter(formatter)
 
-        return cls.log
+    logger.addHandler(handler_std)
+    logger.addHandler(handler_log)
+    return logger
 
+
+log = get_log()
 
