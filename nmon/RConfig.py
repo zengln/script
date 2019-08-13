b20b1416b563f22c18fdc2dcc8200b9a5d2fa62b
zengln <zengln@zln.com>
2019-08-13 21:12:22 +0800

    检测到配置文件不存在,直接退出

--- nmon/RConfig.py
+++ nmon/RConfig.py
@@ -1,13 +1,21 @@
 # -*- coding:utf-8 -*-
 
 import configparser
+import os
+import sys
+
+from nmon.NmonLog import log
 
 
 class Config(object):
 
     def __init__(self):
         self.conf = configparser.ConfigParser()
-        self.conf.read(".\\config\\config.ini", encoding="utf-8-sig")
+        if os.path.exists(".\\config\\config.ini"):
+            self.conf.read(".\\config\\config.ini", encoding="utf-8-sig")
+        else:
+            log.error("请确认配置文件 config.ini 是否存在")
+            sys.exit()
 
     def reload_all_value(self):
         sections = self.conf.sections()
