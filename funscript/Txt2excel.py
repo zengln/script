9a0430fb4768c3297ad1e9fc69dfcca7bbd9e8df
zengln <zengln@zln.com>
2019-08-14 20:43:46 +0800

    新增文件写入

--- funscript/Txt2excel.py
+++ funscript/Txt2excel.py
@@ -5,9 +5,12 @@
 # @Filename: test.py
 
 import os
+import xlrd
+import xlwt
+from xlutils.copy import copy
 
-excle_path = r'D:\MyDocument\nmon\新建文件夹\excel文件夹'
-txt_path = r'D:\MyDocument\nmon\新建文件夹\txt文件'
+excle_path = r'D:\tmp\新建文件夹\excel文件夹'
+txt_path = r'D:\tmp\新建文件夹\txt文件'
 
 
 def get_all_file(path):
@@ -70,11 +73,29 @@ def print_unmatch_file(unmatch_list):
 
 
 def write_to_excel(match_list):
-    pass
+    """
+    将匹配到的 txt 文件内容写入对应 excel
+    :param match_list: 匹配到的文件列表
+    :return: None
+    """
+    for match_file in match_list:
+        if len(match_file) < 2:
+            continue
 
+        old_excel = xlrd.open_workbook(excle_path + "\\" + match_file[1])
+        new_excel = copy(old_excel)
+        ws = new_excel.get_sheet(0)
+
+        txt_files = match_file[1:]
+        for txt_file in txt_files:
+            # TODO zengln: 循环读取 txt 文件内容,区分新增,修改和删除文件, 查找到对应 excel 文件中的列, 并写入数据
+            pass
+
+        new_excel.save(excle_path + "\\" + match_file[1])
 
 excel_files = get_all_file(excle_path)
 txt_files = get_all_file(txt_path)
 match_list, un_match_list = txt_match_excel(txt_list=txt_files[1], excel_list=excel_files[1])
 print(match_list)
 print(un_match_list)
+write_to_excel(match_list)
