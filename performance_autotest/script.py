8ec7e23f9627b41abfaaa6db97b3c4b13e92b119
zengln <zengln@zln.com>
2019-08-22 16:54:04 +0800

    更换为自定义异常

--- performance_autotest/script.py
+++ performance_autotest/script.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 import os
+from performance_autotest.customexception import CustomError
 
 
 def get_all_script(script_path, file_extension):
@@ -12,8 +13,8 @@ def get_all_script(script_path, file_extension):
     """
     script_files = []
     if not os.path.exists(script_path):
-        # TODO 抛出自定义异常,并退出, 将报错信息打印到日志和控制台
-        pass
+        raise CustomError("路径错误,文件夹不存在")
+
     files = os.listdir(script_path)
     for file in files:
         if not os.path.isfile(script_path + "\\" + file):
@@ -22,15 +23,11 @@ def get_all_script(script_path, file_extension):
             script_files.append(os.path.splitext(file)[0])
 
     if not script_files.__len__():
-        # TODO 抛出自定义异常, 路径下无脚本
-        raise Exception
+        raise CustomError("路径下无后缀为%s的脚本文件" % file_extension)
 
     return script_files
 
 
 if __name__ == '__main__':
-    jmeter_path = r'C:\Users\zeng\Desktop\jemter'
+    jmeter_path = r'C:\Users\zengjn\Desktop\jemter'
     print(get_all_script(jmeter_path, ".jmx"))
-
-
-
