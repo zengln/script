9b05f5c891dc3c4d8fc589e9c016f771703b038d
zengln <zengln@zln.com>
2019-08-22 20:58:24 +0800

    自动拼接命令

--- performance_autotest/script.py
+++ performance_autotest/script.py
@@ -9,7 +9,7 @@ def get_all_script(script_path, file_extension):
     获取当前文件夹下所有指定后缀文件
     :param script_path:    文件夹路径
     :param file_extension: 文件类型
-    :return:
+    :return:返回脚本文件列表
     """
     script_files = []
     if not os.path.exists(script_path):
@@ -28,6 +28,41 @@ def get_all_script(script_path, file_extension):
     return script_files
 
 
+def jmeter_cmd(script_file):
+    """
+    获取路径生成执行脚本命令
+    """
+    cmd_list = []
+    # TODO cmd 参数化
+    cmd = r"D:\JMeter\apache-jmeter-5.1.1\bin\jmeter -n -t "
+    for file in script_file:
+        command = cmd + jmeter_path + os.path.sep + file + ".jmx" + " -l " + jmeter_path + os.path.sep + file + ".jtl"
+        print(command)
+        cmd_list.append(command)
+
+    return cmd_list
+
+
+def lr_cmd(script_file):
+    """
+    获取路径生成执行脚本命令
+    """
+    cmd_list = []
+    # TODO cmd 参数化
+    cmd = r'C:\"Program Files (x86)"\HP\LoadRunner\bin\wlrun -TestPath  '
+    for file in script_file:
+        command = cmd + lr_path + os.path.sep + file + ".lrs" + " -Run -ResultName " + lr_path + os.path.sep + file
+        print(command)
+        cmd_list.append(command)
+
+    return cmd_list
+
+
 if __name__ == '__main__':
+    lr_path = r"C:\Users\zengjn\Desktop\Get\scenario"
     jmeter_path = r'C:\Users\zengjn\Desktop\jemter'
-    print(get_all_script(jmeter_path, ".jmx"))
+    files_jmeter = get_all_script(jmeter_path, ".jmx")
+    jmeter_command = jmeter_cmd(files_jmeter)
+    print("lr=============================")
+    lr_files = get_all_script(lr_path, ".lrs")
+    lr_command = lr_cmd(lr_files)
