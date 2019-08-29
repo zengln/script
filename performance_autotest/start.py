63ba93b1e4b81448a9af11de7f114489051e89db
zengln <zengln@zln.com>
2019-08-29 09:49:41 +0800

    优化:支持监控服务器集群

--- performance_autotest/start.py
+++ performance_autotest/start.py
@@ -18,7 +18,7 @@ from performance_autotest.server import Server
 # TODO 获取场景
 # TODO 连接后台
 # TODO 启动场景
-# TODO 启动监控
+# TODO 启动监控(需要支持集群)
 # TODO 所有场景结束
 # TODO 下载监控文件
 # TODO 解析监控文件、结果文件
@@ -31,26 +31,46 @@ def check_dir(path):
         os.makedirs(path)
 
 
+def servers_connect(server_list):
+    num = int(config.remote_host_num)
+    for server_index in range(0, num):
+        server = Server(config.__getattribute__("ip" + str(server_index)))
+        server.connect(config.__getattribute__("user" + str(server_index)),
+                       config.__getattribute__("passwd" + str(server_index)))
+        server_list.append(server)
+
+
+def servers_start_nmon_control(script_file_name):
+    num = int(config.remote_host_num)
+    for server_index in range(0, num):
+        servers[server_index].start_nmon_control(config, script_file_name)
+
+
+def servers_close(server_list):
+    num = int(config.remote_host_num)
+    for server_index in range(0, num):
+        server_list[server_index].download_nmon_files(config)
+        server_list[server_index].close()
+
+
 check_dir(config.download_local_path)
 
 if not config.jmeter_script_dir == "":
     script_path = config.jmeter_script_dir
     script_file = get_all_script(script_path, ".jmx")
-    script_command = jmeter_cmd(script_file)
+    script_command = jmeter_cmd(script_file, config.jmeter_script_dir)
 elif not config.loadrunner_script_dir == "":
     script_path = config.loadrunner_script_dir
     script_file = get_all_script(script_path, ".lrs")
-    script_command = lr_cmd(script_file)
+    script_command = lr_cmd(script_file, config.loadrunner_script_dir)
 else:
     raise CustomError("脚本路径不能全为空")
 
-
-server = Server(config.ip)
-server.connect(config.user, config.passwd)
+servers = []
+servers_connect(servers)
 
 for index, command in script_command:
     exe_command(command)
-    server.start_nmon_control(config, script_file[index])
+    servers_start_nmon_control(script_file[index])
 
-server.download_nmon_files(config)
-server.close()
+servers_close(servers)
