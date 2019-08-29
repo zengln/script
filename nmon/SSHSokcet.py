df980c6aa61097c1701d3ea9c2ec100de1a86374
zengln <zengln@zln.com>
2019-08-29 13:45:45 +0800

    替换下载的 nmon 监控文件根目录名称为服务器ip

--- nmon/SSHSokcet.py
+++ nmon/SSHSokcet.py
@@ -16,6 +16,7 @@ class sshSocket(object):
         tran = paramiko.Transport((hostname, 22))
         tran.connect(username=username, password=password)
         self.sftp = paramiko.SFTPClient.from_transport(tran)
+        self.hostname = hostname
 
     def get_all_file(self, path, basepath, filelist):
 
@@ -23,15 +24,16 @@ class sshSocket(object):
 
         if basepath == path:
             last_index = basepath.rfind("/")
+
             root = basepath[last_index+1:]
             basepath = basepath[:last_index]
+
             if root == "":
                 last_index = basepath[:last_index].rfind("/")
                 root = basepath[last_index+1:]
                 basepath = basepath[:last_index]
         else:
              root = path[basepath.__len__() + 1:]
-
 
         for file in files:
             if S_ISDIR(file.st_mode):
@@ -45,12 +47,21 @@ class sshSocket(object):
 
         return filelist
 
-
     def download_file(self,filespath, localpath, remotepath):
+        log.info("======开始下载" + self.hostname + "上的监控文件=======")
+
         if not os.path.exists(localpath):
             os.makedirs(localpath)
 
+        # 替换下载的 nmon 监控文件根目录名称为服务器ip
+        winfilespath = []
+
         for filepath in filespath:
+            index = filepath.find("/")
+            winfilespath.append(self.hostname + filepath[index:])
+
+
+        for filepath in winfilespath:
             winfilepath = localpath + "\\" + filepath.replace("/", "\\")
             winpathindex = winfilepath.rfind("\\")
             winpath = winfilepath[:winpathindex]
@@ -63,6 +74,19 @@ class sshSocket(object):
             file.close()
             self.sftp.get(remotepath + "/" + filepath[root_index:], winfilepath)
 
+        log.info("======" + self.hostname + "监控文件下载完成=======")
+
     def close(self):
         self.sftp.close()
 
+
+if __name__ == "__main__":
+    hostname = "192.168.36.169"
+    uesrname = "gaps41"
+    password = "gaps41.123"
+    remotePath = r"/home/gaps41/file/sstb/nmon"
+    localPath = r"D:\tmp"
+    ssh = sshSocket(hostname=hostname, username=uesrname, password=password)
+    files = ssh.get_all_file(remotePath, remotePath, [])
+    ssh.download_file(files, localPath, remotePath)
+    ssh.close()
