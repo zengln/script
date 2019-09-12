c134d9360204ac577bf19e03e758ee842ae4ad00
zengln <zengln@zln.com>
2019-09-12 20:03:09 +0800

    整合所有步骤,调整总脚本

--- performance_autotest/start.py
+++ performance_autotest/start.py
@@ -13,6 +13,7 @@ from performance_autotest.RConfig import config
 from performance_autotest.server import Server
 from performance_autotest.log import logger
 from performance_autotest.resultdata import NmonAnalyse, JmeterAnalyse, LoadRunnerAnalyse
+from performance_autotest.report import Report
 
 """
     入口脚本
@@ -68,37 +69,42 @@ def analyse_nmon(server_list, nmon_result_list):
     for server in server_list:
         for filepath in server.file_list:
             nmon = NmonAnalyse()
+            nmon.set_ip(server.server_name)
             nmon.file_analyse(filepath)
             nmon_result_list.append(nmon)
 
     logger.info("解析nmon文件结束")
-    return nmon_result_list
 
 
-def analyse_jmeter(jmeter_result_list):
+def analyse_jmeter(jmeter_file_list, jmeter_result_list):
     logger.info("开始解析jmeter文件")
 
-    for jmeter_file in jmeter_result_list:
+    for jmeter_file in jmeter_file_list:
         jmeter = JmeterAnalyse()
         jmeter.file_analyse(jmeter_file)
+        jmeter_result_list.append(jmeter)
 
     logger.info("解析jmeter文件结束")
 
 
-def analyse_loadrunner(loadrunner_result_list):
+def analyse_loadrunner(loadrunner_file_list, loadrunner_result_list):
     logger.info("开始解析loadrunner文件")
 
-    for loadrunner_file in loadrunner_result_list:
+    for loadrunner_file in loadrunner_file_list:
         loadrunner = LoadRunnerAnalyse()
         loadrunner.file_analyse(loadrunner_file)
+        loadrunner_result_list.append(loadrunner)
 
     logger.info("解析loadrunner文件结束")
 
 try:
     # 保存结果文件路径
-    result_nmon_list = []
-    result_jmeter_list = []
-    result_loadrunner_list = []
+    result_jmeter_file_list = []
+    result_loadrunner_file_list = []
+
+    # 保存解析文件结果
+    result_nmon_variable_list = []
+    result_file_analyse_variable_list = []
 
     check_dir(config.download_local_path)
 
@@ -106,11 +112,11 @@ try:
     if not config.jmeter_script_dir == "":
         script_path = config.jmeter_script_dir
         script_file = get_all_script(script_path, ".jmx")
-        script_command, result_jmeter_list = jmeter_cmd(script_file, config.jmeter_script_dir)
+        script_command, result_jmeter_file_list = jmeter_cmd(script_file, config.jmeter_script_dir)
     elif not config.loadrunner_script_dir == "":
         script_path = config.loadrunner_script_dir
         script_file = get_all_script(script_path, ".lrs")
-        script_command, result_analyse_command, result_loadrunner_list = lr_cmd(script_file, config.loadrunner_script_dir)
+        script_command, result_analyse_command, result_loadrunner_file_list = lr_cmd(script_file, config.loadrunner_script_dir)
     else:
         raise CustomError("脚本路径不能全为空")
 
@@ -132,20 +138,20 @@ try:
         for command in result_analyse_command:
             exe_command(command)
 
-    analyse_nmon(servers, result_nmon_list)
+    analyse_nmon(servers, result_nmon_variable_list)
     if not config.jmeter_script_dir == "":
-        if len(result_jmeter_list) == 0:
+        if len(result_jmeter_file_list) == 0:
             raise CustomError("jmeter 解析时出现异常,找不到结果文件所在路径")
-        analyse_jmeter(result_jmeter_list)
+        analyse_jmeter(result_jmeter_file_list, result_file_analyse_variable_list)
     elif not config.loadrunner_script_dir == "":
-        if len(result_loadrunner_list) == 0:
+        if len(result_loadrunner_file_list) == 0:
             raise CustomError("loadrunner 解析时出现异常,找不到结果文件所在路径")
-        analyse_loadrunner(result_loadrunner_list)
+        analyse_loadrunner(result_loadrunner_file_list, result_file_analyse_variable_list)
     else:
         raise CustomError("脚本路径不能全为空,解析结果失败")
 
-    # TODO 生成报告
-    print("生成报告")
+    report = Report()
+    report.get_report(result_file_analyse_variable_list, result_nmon_variable_list)
 except Exception:
     error_msg = traceback.format_exc()
     logger.error(error_msg)
