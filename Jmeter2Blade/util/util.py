616cc2fb69d4550d6f12db1b4cc75852a71dece4
zengln <zengln@zln.com>
2021-03-18 17:00:50 +0800

    工具类

---
+++ Jmeter2Blade/util/util.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+# @Time    : 2021/3/18 10:46
+# @Author  : zengln
+# @File    : util.py
+
+'''
+工具类
+
+后续考虑是否放入配置文件, 工具类不对外开放
+'''
+
+TOKEN = ""
+account = ""
+dealVariableData_url = ""
+importOfflineCase_url = ""
