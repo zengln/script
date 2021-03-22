e0dce83b62c8a6fae8311b21a957cc516d679fd3
zengln <zengln@zln.com>
2021-03-22 19:32:51 +0800

    自定义变量组件解析完成

--- Jmeter2Blade/adapter/JcsvAdapter.py
+++ Jmeter2Blade/adapter/JcsvAdapter.py
@@ -3,18 +3,57 @@
 # @Author  : zengln
 # @File    : JcsvAdapter.py
 
-
+import os
 import xml.etree.ElementTree as ET
 
+from Jmeter2Blade.socket.PostBlade import VariableData, PostBlade
+from Jmeter2Blade.util.log import logger
 
-tree = ET.parse('movie.xml')
+# 自定义变量组件处理
+def deal_arguments(root, node_name):
+    post_blade = PostBlade()
+    variable_date = VariableData(node_name)
+    for child in root[0]:
+        data = {}
+        for sub_child in child:
+            if sub_child.attrib['name'] == "Argument.name":
+                data["varName"] = sub_child.text
+            elif sub_child.attrib['name'] == "Argument.value":
+                data["varContent"] = sub_child.text
+            elif sub_child.attrib['name'] == "Argument.desc":
+                data["variableRemark"] = sub_child.text
+        variable_date.set_date(data)
+
+    logger.info(post_blade.dealVariableData(variable_date))
+
+# 线程组组件
+def deal_threadgroup(root):
+    pass
+
+
+# 定义blade根路径
+root_name = "jmeter转blade测试"
+# 读取xml文件
+tree = ET.parse('movie.jmx')
+# 获取xml根节点
 root = tree.getroot()
+# 找到TestPlan组件, 获取blade二级路径名
+test_plan = root[0].findall('TestPlan')
+logger.info(test_plan)
+root_node_name = test_plan[0].get("testname")
+# 获取jmx文件根节点
 jmx_root = root[0][1]
 
+
 for child in jmx_root:
-    if child.tag != "hashTree":
-        print(child.tag, child.attrib)
-
-
-if __name__ == "__main__":
-    pass
+    if child.tag != "hashTree" and child.attrib['enabled'] == "true":
+        # print(child.tag, child.attrib)
+        # 自定义变量组件处理
+        if child.tag == "Arguments":
+            # jmeter转blade测试/iibs/自定义变量
+            name = root_name + os.path.altsep + root_node_name + os.path.altsep + child.get("testname")
+            logger.info(name)
+            # deal_arguments(child, node_name=name)
+        # 线程组组件处理
+        elif child.tag == "ThreadGroup":
+            deal_threadgroup(child)
