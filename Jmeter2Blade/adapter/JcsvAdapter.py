9e4c8fd5c8ddb67ad057cbffa9bb1a297defd483
zengln <zengln@zln.com>
2021-03-24 20:00:27 +0800

    ThreadGroup组件处理

--- Jmeter2Blade/adapter/JcsvAdapter.py
+++ Jmeter2Blade/adapter/JcsvAdapter.py
@@ -8,6 +8,8 @@ import xml.etree.ElementTree as ET
 
 from Jmeter2Blade.socket.PostBlade import VariableData, PostBlade
 from Jmeter2Blade.util.log import logger
+from Jmeter2Blade.util.JmeterElement import JmeterElement
+
 
 # 自定义变量组件处理
 def deal_arguments(root, node_name):
@@ -28,12 +30,13 @@ def deal_arguments(root, node_name):
 
 
 # 线程组组件
-def deal_threadgroup(root, node_name):
-    for child in root:
-        # 处理HTTP请求
-        if child.tag == "HTTPSamplerProxy":
-            # TODO 解析这个组件,组装请求报文
-            pass
+def deal_threadgroup(root, name):
+    sub_elements = root.get_sub_elements()
+    for sub_element in sub_elements:
+        if sub_element.tag == "HTTPSamplerProxy":
+            request_body = sub_element.element.find(".//stringProp[@name='Argument.value']").text
+
+
 
 
 # 定义blade根路径
@@ -42,33 +45,27 @@ root_name = "jmeter转blade测试"
 tree = ET.parse('movie.jmx')
 # 获取xml根节点
 root = tree.getroot()
+
 # 找到TestPlan组件, 获取blade二级路径名
-test_plan = root[0].findall('TestPlan')
+test_plan = root[0][0]
 logger.info(test_plan)
-root_node_name = test_plan[0].get("testname")
+root_node_name = test_plan.get("testname")
 # 获取jmx文件根节点
 jmx_root = root[0][1]
-# 记录上一个组件
-last_child = None
-last_child_enabled = False
 
-for child in jmx_root:
-    if child.get("enabled", "false") == "true" or (child.tag == "hashTree" and last_child_enabled):
+companents = JmeterElement(test_plan, jmx_root).get_sub_elements()
+
+for companent in companents:
+    if companent.isEnabled():
         # 自定义变量组件处理
-        if child.tag == "Arguments":
+        if companent.tag == "Arguments":
             # jmeter转blade测试/iibs/自定义变量
-            name = root_name + os.path.altsep + root_node_name + os.path.altsep + child.get("testname")
+            name = root_name + os.path.altsep + root_node_name + os.path.altsep + companent.get("testname")
             logger.info(name)
             # deal_arguments(child, node_name=name)
-        # 线程组组件处理
-        elif child.tag == "ThreadGroup":
-            last_child = "ThreadGroup"
-            last_name = child.get("testname")
-            last_child_enabled = True
-        # hashTree 处理, Thread 线程组件中, 实际需要提取的数据信息都放在相邻的 hashTree 中
-        elif child.tag == "hashTree":
-            last_child_enabled = False
-            if last_child == "ThreadGroup":
-                name = root_name + os.path.altsep + root_node_name + os.path.altsep + last_name
-                deal_threadgroup(child, name)
+        elif companent.tag == "ThreadGroup":
+            logger.info(companent.get("testname"))
+            name = root_name + os.path.altsep + root_node_name + os.path.altsep + companent.get("testname")
+            deal_threadgroup(companent, name)
+
 
