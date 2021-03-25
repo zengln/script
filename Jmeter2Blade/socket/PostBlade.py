807ac1b1fcfce813348081be5eadf3da7f919bc4
zengln <zengln@zln.com>
2021-03-25 14:13:05 +0800

    增加脚本处理接口

--- Jmeter2Blade/socket/PostBlade.py
+++ Jmeter2Blade/socket/PostBlade.py
@@ -11,7 +11,9 @@ from Jmeter2Blade.util.log import logger
 
 
 class VariableData:
-
+    """
+    变量数据处理接口
+    """
     def __init__(self, dataNode, data=[], vid="", vName=""):
         self.dataNode = dataNode
         self.data = data
@@ -39,18 +41,46 @@ class importOfflineCase:
 
 
 class dealScriptData:
-
-    def __init__(self, dataNode, data, vid="", vName=""):
-        self.dataNode = dataNode
+    """
+    脚本处理接口, 可以自行传入数据, 或者使用默认模版(后续可能调整)
+    """
+    def __init__(self, data_node, data=[], vid="", vName=""):
+        self.dataNode = data_node
         self.data = data
         self.vid = vid
         self.vName = vName
         self.account = util.account
         self.api_token = util.TOKEN
 
+    def set_data(self, data):
+        if not isinstance(data, dict):
+            return False
+
+        self.data.append(data)
+
+    def set_data_with_default(self, script_name, root_url, path, script_remark=""):
+        data = {
+          "scriptName": "%s" % script_name,
+          "scriptContent": "http|%s,%s,POST,UTF-8,json,Content-Type=application/json;charset=UTF-8,,300" % (root_url, path),
+          "scriptRemark": "%s" % script_remark,
+          "scriptJsonData": {"根URL": "%s" % root_url, "路径": "%s" % path, "请求方式": "POST", "编码类型": "UTF-8", "返回形式": "json",
+                             "header配置": "Content-Type=application/json;charset=UTF-8", "精准标签": "",
+                             "超时时间": "300"}
+        }
+        logger.info(data)
+        self.data.append(data)
+
 
 class PostBlade:
+    """
+    与Blade通信, 将发送与接收返回信息后的数据提取公共化
+    """
     SUCCESS = 'success'
+
+    proxies = {
+        "http": 'http://127.0.0.1:8080',
+        "https": 'http://127.0.0.1:8080'
+    }
 
     def __init__(self):
         self.headers = {
@@ -70,7 +100,6 @@ class PostBlade:
         else:
             return resp_content_json['sub_msg']
 
-
     # blade 接口-添加测试用例
     def importOfflineCase(self, data):
         if not isinstance(data, importOfflineCase):
@@ -84,12 +113,29 @@ class PostBlade:
         else:
             return resp_content_json['sub_msg']
 
+    # blade 接口-脚本数据处理接口
+    def dealScriptData(self, data):
+        if not isinstance(data, dealScriptData):
+            return "data 需要为 dealScriptData 实例"
+        resp = requests.post(util.dealScriptData_url, data=json.dumps(data.__dict__), headers=self.headers)
+        content = resp.content.decode("utf-8")
+        resp_content_json = json.loads(content)
+        logger.info(resp_content_json)
+        if resp_content_json['msg'] == PostBlade.SUCCESS and resp_content_json['sub_msg'] == "":
+            return
+        else:
+            return resp_content_json['sub_msg']
 
 
 if __name__ == "__main__":
-    vd = VariableData("自定义变量", [{"varName": "varc_test", "varContent": 3, "variableRemark":"python"}])
+    # vd = VariableData("自定义变量", [{"varName": "varc_test", "varContent": 3, "variableRemark":"python"}])
+    # pb = PostBlade()
+    # pb.dealVariableData(vd)
+    ds = dealScriptData(data_node="jmeter转blade测试/")
+    ds.set_data_with_default(script_name="FROMPYTHON", root_url="iibs_config", path="/python", script_remark="Python备注")
     pb = PostBlade()
-    pb.dealVariableData(vd)
+    logger.info(pb.dealScriptData(ds))
+
 
 
 
