6ec21b0c5de96edb2e38713c44cb8f859a6b9ac1
zengln <zengln@zln.com>
2021-03-26 11:19:04 +0800

    添加生成指定长度字符串工具函数

--- Jmeter2Blade/util/util.py
+++ Jmeter2Blade/util/util.py
@@ -14,3 +14,17 @@ account = ""
 dealVariableData_url = ""
 importOfflineCase_url = ""
 dealScriptData_url = ""
+
+
+def random_uuid(length:int) ->str:
+    """
+    随机生成一个包含数字字母的指定长度随机字符串
+    :param length: 生成随机字符串的长度
+    :return: 随机字符串
+    """
+    import random, string
+    return ''.join(random.choice(string.digits + string.ascii_letters) for _ in range(length))
+
+
+if __name__ == "__main__":
+    print(random_uuid(32))
