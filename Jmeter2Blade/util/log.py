b24efa1dbbc699af49d37c749b37c61b8c117efc
zengln <zengln@zln.com>
2021-05-20 10:38:06 +0800

    log显示优化，不同级别的log显示不同颜色

--- Jmeter2Blade/util/log.py
+++ Jmeter2Blade/util/log.py
@@ -3,7 +3,19 @@
 # @Author  : zengln
 # @File    : log.py
 
+from colorlog import ColoredFormatter
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(filename)s - %(lineno)d - %(levelname)s - %(message)s')
+LOG_LEVEL = logging.INFO
+
+
+log_format = "%(log_color)s%(asctime)s - %(filename)s - %(lineno)d - %(levelname)s - %(message)s%(reset)s"
+
+colored_formatter = ColoredFormatter(log_format)
 logger = logging.getLogger("Jmeter2Blade")
+stream = logging.StreamHandler()
+stream.setLevel(LOG_LEVEL)
+stream.setFormatter(colored_formatter)
+
+logger.addHandler(stream)
+logger.setLevel(LOG_LEVEL)
